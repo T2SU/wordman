fdecc4ec2e7e6018198794381da9e310aa62434c
T2SU <seria0no@gmail.com>
2018-07-25 00:40:41 +0900

    주석2차.. 생각날때 주석은 계속 적겠습니다..

--- word.py
+++ word.py
@@ -5,20 +5,24 @@ from setup import curtime
 # 단어 관련 클래스 집합 스크립트
 
 
+# 새로운 단어 관리 클래스
 def init_words(db_name):
     return Words(db_name)
 
+# 새로운 Example 관리 클래스
 def init_examples(db_name):
     return ExampleWordContext(db_name)
 
+#새로운 Synonym 관리 클래스
 def init_synonyms(db_name):
     return SynonymWordContext(db_name)
 
+#새로운 Antonym 관리 클래
 def init_antonyms(db_name):
     return AntonymWordContext(db_name)
 
 
-# Words 클래스
+# Words 클래스. 단어들을 관리하는 클래스임.
 
 class Words:
     def __init__(self, name):
@@ -30,34 +34,41 @@ class Words:
             curs.execute('create table if not exists `synonyms` (`word` text not null, `synonym` text)')
             curs.execute('create table if not exists `antonyms` (`word` text not null, `antonym` text)')
         logging.debug('[words] ready db')
-        
+
+    # 새로운 단어 등록.
     def new_word(self, word):
         with sqlite3.connect(self.db_name + '.db', check_same_thread = False) as conn:
             curs = conn.cursor()
             curs.execute('insert into `words` (`word`, `ref`, `lastref`) values (?, ?, ?)', [word, 0, curtime()])
 
+    # 등록된 단어 디비에서 불러오기
     def load_words(self, _orderby, _d, _page):
         with sqlite3.connect(self.db_name + '.db', check_same_thread = False) as conn:
             curs = conn.cursor()
 
+            # 기본값
             page = 1
             orderby = 'word'
             d = 'asc'
-            
+
+            # _page 값이 있으면 (= None 이 아니면) 그 값을 가져옴.
             if (_page is not None):
                 page = int(_page)
-
-            if (_orderby is not None or _d is not None):
+                
+            # _orderby와 _d 값이 있으면 (= None 이 아니면) 그 값을 가져옴.
+            if (_orderby is not None and _d is not None):
                 orderby = str(_orderby)
                 d = str(_d)
 
+            # 한 페이지에 50개의 단어
             s = (page - 1) * 50
                 
             query = 'select `word`, `ref`, `lastref` from `words` order by `' + orderby + '` collate nocase ' + d + ' limit ' + str(s) + ', 50';
             curs.execute(query)
             ret = curs.fetchall()
             return ret
-        
+
+# Examples, Synonyms, Antonyms 등 단어의 세부 항목들을 관리하는데 사용할 기본 클래스. 필요한 항목에서 상속하여 사용한다.
 class WordContext:
     def __init__(self, dbname, name):
         self.name = name
@@ -67,6 +78,15 @@ class WordContext:
             curs.execute('create table if not exists `' + self.name + 's` (`word` text not null, `' + self.name + '` text)')
         logging.debug('[' + name + 's] ready db')
 
+    # 단어 추가/수정/삭제.
+    #
+    # 추가:
+    #   oldone값이 없고, newone 값이 있다.
+    # 수정:
+    #   oldone값이 있고, newone 값이 있다.
+    # 삭제:
+    #   oldone값이 있고, newone 값이 없다.
+    #
     def change(self, word, oldone, newone):
         with sqlite3.connect(self.db_name + '.db', check_same_thread = False) as conn:
             curs = conn.cursor()
@@ -78,7 +98,7 @@ class WordContext:
                 curs.execute('insert into `' + self.name + 's` (`word`, `' + self.name + '`) values (?, ?)', [word, n_one])
             else:
                 curs.execute('delete from `' + self.name + 's` where `word` = ? and `' + self.name + '` = ?', [word, o_one])
-        
+    # 해당 단어의 현재 클래스가 관리하는 세부 항목을 가져온다.
     def get(self, word):
         with sqlite3.connect(self.db_name + '.db', check_same_thread = False) as conn:
             curs = conn.cursor()
@@ -87,6 +107,10 @@ class WordContext:
             if got_data:
                 return got_data
             return []
+
+
+
+# 세부 항목 상속 클래스
 
 class ExampleWordContext(WordContext):
     def __init__(self, dbname):
